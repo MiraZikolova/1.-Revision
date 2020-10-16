94bfba11e21b903b6eb674c331daab3bbf6300e9
MiraZikolova <71563228+MiraZikolova@users.noreply.github.com>
2020-10-16 15:06:20 +0300

    Add files via upload

---
+++ Casting01.java
@@ -0,0 +1,28 @@
+package projects04;
+
+public class Casting01 {
+    public static void main(String[] args) {
+        //Declare and initialize a byte with a value of 128
+        //Observe NetBeans' complaint
+    	byte task = 127;
+        
+        //Declare and initialize a short with a value of 128
+        //Create a print statement that casts this short to a byte
+    	
+    	short zad = 130;
+    	System.out.println((byte)zad);
+    	
+        
+        
+        //Declare and initialize a byte with a value of 127
+        //Add 1 to this variable and print it
+        //Add 1 to this variable again and print it again
+    	byte test = 127;
+    	test++;
+    	System.out.println(test);
+    	
+        
+        
+        
+    }    
+}
