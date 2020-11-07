e68e181bb5ff1a024c0c1544e8bb17314406d46b
MiraZikolova <71563228+MiraZikolova@users.noreply.github.com>
2020-11-07 13:55:10 +0200

    Add files via upload

---
+++ PrivateCircle12.java
@@ -0,0 +1,5 @@
+package modifiers12;
+
+public class PrivateCircle12 {
+
+}
---
+++ PrivateCircleTest12.java
@@ -0,0 +1,10 @@
+package modifiers12;
+
+public class PrivateCircleTest12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
